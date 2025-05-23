84ed323c4714af277a1c91f6ba4320c1914c0c0a
Agrima4 <43441097+Agrima4@users.noreply.github.com>
2025-05-22 20:22:57 -0400

    Update env.ts
    Hardcode environment values for testing

--- src/env.ts
+++ src/env.ts
@@ -11,16 +11,23 @@
 // export const API_TOKEN = 'yPWaWARdLvaKZd0blo3cjBcZBNwZE3t2ghzSs6Rf';
 
 
-export const CLOUDMR_SERVER = process.env.CLOUDMR_SERVER || 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
+// export const CLOUDMR_SERVER = process.env.CLOUDMR_SERVER || 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
 
-export const MRO_SERVER = process.env.MRO_SERVER || 'https://w3k2wkjkdl.execute-api.us-east-1.amazonaws.com';
+// export const MRO_SERVER = process.env.MRO_SERVER || 'https://w3k2wkjkdl.execute-api.us-east-1.amazonaws.com';
 
-// To be merged into CloudMR Server
-export const PROFILE_SERVER = process.env.PROFILE_SERVER || 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
+// // To be merged into CloudMR Server
+// export const PROFILE_SERVER = process.env.PROFILE_SERVER || 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
 
-export const API_URL = process.env.API_URL || 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
+// export const API_URL = process.env.API_URL || 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
 
-export const API_TOKEN = process.env.API_TOKEN || 'khZdn001QNpOyQVxXRCS2KZezlJDnVdL'; // Replace with fresh token if needed
+// export const API_TOKEN = process.env.API_TOKEN || 'khZdn001QNpOyQVxXRCS2KZezlJDnVdL'; // Replace with fresh token if needed
+
+export const CLOUDMR_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
+export const MRO_SERVER = 'https://w3k2wkjkdl.execute-api.us-east-1.amazonaws.com';
+export const PROFILE_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
+export const API_URL = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
+export const API_TOKEN = 'khZdn001QNpOyQVxXRCS2KZezlJDnVdL'; // Replace with a fresh token if this expires
+
 
 
 
