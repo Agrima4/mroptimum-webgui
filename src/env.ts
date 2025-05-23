9b8a00c3748413f324f1b0e674761b2687e02676
Agrima4 <43441097+Agrima4@users.noreply.github.com>
2025-05-23 10:56:00 -0400

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -22,11 +22,12 @@
 
 // export const API_TOKEN = process.env.API_TOKEN || 'khZdn001QNpOyQVxXRCS2KZezlJDnVdL'; // Replace with fresh token if needed
 
-export const CLOUDMR_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
-export const MRO_SERVER = 'https://w3k2wkjkdl.execute-api.us-east-1.amazonaws.com';
-export const PROFILE_SERVER = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com';
-export const API_URL = 'https://ewjjq013u0.execute-api.us-east-1.amazonaws.com/api-token';
-export const API_TOKEN = 'khZdn001QNpOyQVxXRCS2KZezlJDnVdL'; // Replace with a fresh token if this expires
+export const CLOUDMR_SERVER = 'https://kup9r3det1.execute-api.us-east-1.amazonaws.com'; // CloudMR backend
+export const MRO_SERVER = 'https://w3k2wkjkdl.execute-api.us-east-1.amazonaws.com'; // MROptimum API Gateway
+export const PROFILE_SERVER = 'https://kup9r3det1.execute-api.us-east-1.amazonaws.com'; // Can be the same as CLOUDMR if used for user profile
+export const API_URL = 'https://kup9r3det1.execute-api.us-east-1.amazonaws.com/Prod/api-token'; // api-token route in CloudMR (adjust if path differs)
+export const API_TOKEN = ''; //  Leave empty for now, should be dynamically fetched on login
+
 
 
 
